ed0e63e04c5cc10602ea782ac7f0e9c8f287effb
BlueSnoopT <bluesnoopbs@gmail.com>
2023-04-12 23:24:12 -0500

    remodel folders and add inject and update
    Full summary:
    
    * Removed git updates
    * Added better injector and updater
    
    This is just better.

--- Project/toolchain.cmake
+++ Project/toolchain.cmake
@@ -1,21 +1,19 @@
-SET(CMAKE_C_FLAGS             "-m32" CACHE STRING "C compiler flags"   FORCE)
-SET(CMAKE_CXX_FLAGS           "-m32" CACHE STRING "C++ compiler flags" FORCE)
+SET(CMAKE_C_FLAGS "-m32" CACHE STRING "C compiler flags"   FORCE)
+SET(CMAKE_CXX_FLAGS "-m32" CACHE STRING "C++ compiler flags" FORCE)
 
-SET(LIB32 /usr/lib) # Fedora
+SET(LIB32 /usr/lib)
 
 IF(EXISTS /usr/lib32)
-    SET(LIB32 /usr/lib32) # Arch, Solus
+    SET(LIB32 /usr/lib32)
 ENDIF()
 
 SET(CMAKE_SYSTEM_LIBRARY_PATH ${LIB32} CACHE STRING "system library search path" FORCE)
 SET(CMAKE_LIBRARY_PATH        ${LIB32} CACHE STRING "library search path"        FORCE)
 
-# this is probably unlikely to be needed, but just in case
 SET(CMAKE_EXE_LINKER_FLAGS    "-m32 -L${LIB32}" CACHE STRING "executable linker flags"     FORCE)
 SET(CMAKE_SHARED_LINKER_FLAGS "-m32 -L${LIB32}" CACHE STRING "shared library linker flags" FORCE)
 SET(CMAKE_MODULE_LINKER_FLAGS "-m32 -L${LIB32}" CACHE STRING "module linker flags"         FORCE)
 
-# find Wx config script on Fedora for the highest version of 32 bit Wx installed
 IF(EXISTS ${LIB32}/wx/config)
     FILE(GLOB WX_INSTALLS ${LIB32}/wx/config/*)
 
@@ -35,17 +33,14 @@ IF(EXISTS ${LIB32}/wx/config)
         ""
     )
     FILE(WRITE ${CMAKE_BINARY_DIR}/wx-config-wrapper
-"#!/bin/sh
-${WX_INSTALL_CONFIG} \"\$@\" | sed 's!/emul32/!/usr/!g'
-")
+        "#!/bin/sh
+        ${WX_INSTALL_CONFIG} \"\$@\" | sed 's!/emul32/!/usr/!g'
+    ")
     EXECUTE_PROCESS(COMMAND chmod +x ${CMAKE_BINARY_DIR}/wx-config-wrapper)
 
     SET(wxWidgets_CONFIG_EXECUTABLE ${CMAKE_BINARY_DIR}/wx-config-wrapper)
 ENDIF()
 
-# on Fedora and Arch and similar, point pkgconfig at 32 bit .pc files. We have
-# to include the regular system .pc files as well (at the end), because some
-# are not always present in the 32 bit directory
 IF(EXISTS ${LIB32}/pkgconfig)
     SET(ENV{PKG_CONFIG_LIBDIR} ${LIB32}/pkgconfig:/usr/share/pkgconfig:/usr/lib/pkgconfig:/usr/lib64/pkgconfig)
 ENDIF()
